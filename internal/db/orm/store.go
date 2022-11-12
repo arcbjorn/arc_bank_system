97fec5f0074e938769a765742009dc8402bcf161
arcbjorn <arky.happ@gmail.com>
2022-11-12 19:07:01 -0300

    feat(db/orm/Store): add Exec transaction fn

--- internal/db/orm/store.go
+++ internal/db/orm/store.go
@@ -1,7 +1,9 @@
 package db
 
 import (
+	"context"
 	"database/sql"
+	"fmt"
 )
 
 // functions to execute queries and transactions
@@ -15,4 +17,25 @@ func NewStore(db *sql.DB) *Store {
 		db:      db,
 		Queries: New(db),
 	}
+}
+
+// executes function within a database transaction
+func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
+	tx, err := store.db.BeginTx(ctx, nil)
+	if err != nil {
+		return nil
+	}
+
+	q := New(tx)
+	err = fn(q)
+
+	if err != nil {
+		if rollbackEror := tx.Rollback(); rollbackEror != nil {
+			return fmt.Errorf("tx error: %v, rollbsack error: %v", err, rollbackEror)
+		}
+
+		return err
+	}
+
+	return tx.Commit()
 }
