0f08dedba1c5050ecd88af4b1ef6742c4a0a4f5f
arcbjorn <arky.happ@gmail.com>
2022-11-12 19:06:12 -0300

    feat(internal/db/orm): add Store

---
+++ internal/db/orm/store.go
@@ -0,0 +1,18 @@
+package db
+
+import (
+	"database/sql"
+)
+
+// functions to execute queries and transactions
+type Store struct {
+	*Queries
+	db *sql.DB
+}
+
+func NewStore(db *sql.DB) *Store {
+	return &Store{
+		db:      db,
+		Queries: New(db),
+	}
+}
