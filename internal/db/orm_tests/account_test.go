9c320e9c4cd86a1ee499aa9109f56929612a4c9e
arcbjorn <arky.happ@gmail.com>
2022-11-12 16:08:05 -0300

    test(db/orm_tests/account_test): Create

---
+++ internal/db/orm_tests/account_test.go
@@ -0,0 +1,32 @@
+package orm_tests
+
+import (
+	"context"
+	"testing"
+
+	orm "github.com/arcbjorn/arc_bank_system/internal/db/orm"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestCreateAccount(t *testing.T) {
+	arg := orm.CreateAccountParams{
+		Owner:    "Tom",
+		Balance:  100,
+		Currency: "USD",
+	}
+
+	account, err := testQueries.CreateAccount(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, account)
+
+	require.Equal(t, arg.Owner, account.Owner)
+	require.Equal(t, arg.Balance, account.Balance)
+	require.Equal(t, arg.Currency, account.Currency)
+
+	require.NotZero(t, account.ID)
+	require.NotZero(t, account.CreatedAt)
+
+	error := testQueries.DeleteAccount(context.Background(), account.ID)
+	require.Nil(t, error)
+}
